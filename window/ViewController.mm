088451889c679538415056ef23585b7aa069b334
Shawn Presser <shawnpresser@gmail.com>
2019-04-01 20:56:09 -0400

    Set random background color on tap

--- window/ViewController.mm
+++ window/ViewController.mm
@@ -116,12 +116,22 @@ void nodeStartAsync(void);
 
 @implementation ViewController
 
+
+
 //The event handling method
 - (void)handleSingleTap:(UITapGestureRecognizer *)recognizer
 {
   CGPoint location = [recognizer locationInView:[recognizer.view superview]];
 
   printf("touch\n");
+
+    
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    
+    [self.view setBackgroundColor:color];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
